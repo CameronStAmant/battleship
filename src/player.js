a50d3a9e9e5c90efb3c34cc044f53f527ae6833d
Cameron St. Amant <cameronstamant@protonmail.com>
2020-12-09 07:46:09 -0700

    computer player randomly chooses coordinates

--- src/player.js
+++ src/player.js
@@ -1,5 +1,18 @@
 const playerFactory = () => {
   const attackEnemy = (coordinate, enemyBoard) => {
+    if (coordinate === null) {
+      let remainingCoordinates = [];
+      for (const [key, value] of Object.entries(enemyBoard.board)) {
+        if (value === 'ship' || value === '') {
+          remainingCoordinates.push(key);
+        }
+      }
+      const randomCoordinate =
+        remainingCoordinates[
+          Math.floor(Math.random() * remainingCoordinates.length)
+        ];
+      return enemyBoard.receiveAttack(randomCoordinate);
+    }
     return enemyBoard.receiveAttack(coordinate);
   };
 
